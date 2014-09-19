f2ae388dfe3dc27ecc3660b9282cbe980c3d08be
support <support@werkstatt-1.ksenge>
2014-09-19 14:08:45 +0200

    hugo had an intendation error

--- hugo.py
+++ hugo.py
@@ -5,13 +5,12 @@
 
 def main():
 	max = int(input('Geben Sie die maximale Anzahl ein: '))
-
 	for i in range(1,max+1):
     		if i%7==0 or str(i).find('7')!=-1:
         		print('Hugo')
     		else:
-        print(i, end=' ')
-input('Press Enter to stop')
+        		print(i, end=' ')
+	input('Press Enter to stop')
 
 
 if __name__ == '__main__':main()
