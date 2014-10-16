23915ea3509c1885880909d7005adab9cc8838eb
Gregor Lüdi <gregor.luedi@ken.ch>
2014-10-16 16:46:32 +0200

    iss to read the cuent position

---
+++ iss-position.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import urllib2
+import json
+
+req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+response = urllib2.urlopen(req)
+
+obj = json.loads(response.read())
+
+print obj['timestamp']
+print obj['iss_position']['latitude']
+print obj['iss_position']['longitude']
